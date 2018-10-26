4ebb213e3be943174db2425b4ff0fb7930b77ae3
Vitaliy Korobkin <flatline@yandex-team.ru>
2018-10-27 02:33:01 +0300

    Add command line to device

--- device.go
+++ device.go
@@ -4,12 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	resp, err := http.Get("http://130.193.56.206/dbtest")
+	app := cli.NewApp()
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "event",
+			Aliases: []string{"e"},
+			Usage:   "send event",
+			Action: func(c *cli.Context) error {
+				resp, err := http.Get("http://130.193.56.206/dbtest")
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Response: %v", resp)
+				return nil
+			},
+		},
+		{
+			Name:    "start",
+			Aliases: []string{"s"},
+			Usage:   "start device",
+			Action: func(c *cli.Context) error {
+				fmt.Println("Device is starting...")
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Response: %v", resp)
 }
--- go.mod
+++ go.mod
@@ -3,4 +3,5 @@ module github.com/gophers-team/gopher-box
 require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gorilla/mux v1.6.2
+	github.com/urfave/cli v1.20.0 // indirect
 )
