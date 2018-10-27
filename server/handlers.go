ed32f02392f94e2baf4b524cd8424079a56fcd2c
Dixie Flatline <vitalisbox@gmail.com>
2018-10-27 16:52:34 +0300

    Status handle stub

--- server/handlers.go
+++ server/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -38,14 +39,29 @@ func heartbeatHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
 	tx := db.MustBegin()
 	tx.MustExec(heartbeatQuery, h.DeviceID, time.Now())
 	tx.Commit()
+
 	w.WriteHeader(http.StatusOK)
 }
 
-func deviceDispenseHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
+func dispenseHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
 	var t api.DeviceTabletDispenseRequest
-	if err := json.NewDecoder(r.Body).Decode(&h); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte("Failed to unmarshal /dispense request"))
 		return
 	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func statusHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
+	var s api.DeviceTabletStatusRequest
+	if err := json.NewDecoder(r.Body).Decode(&s); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Failed to unmarshal /status request"))
+		return
+	}
+
+	fmt.Println(s)
+	w.WriteHeader(http.StatusOK)
 }
