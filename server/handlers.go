dd6e32779da3ab0a206b887cfcf9fa1582cebf0f
Vladimir Kononov <pono@yandex-team.ru>
2018-10-27 13:37:36 +0300

    Add heartbeat handler to server

--- server/handlers.go
+++ server/handlers.go
@@ -35,6 +35,34 @@ func EventsHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "VARS: %v\n", vars)
 }
 
+var heartbeatQuery = `
+INSERT INTO events (
+	device_id,
+	event_type,
+	timestamp,
+	created_at
+)
+VALUES ($1, $2, $3, $3)`
+
+func heartbeatHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Failed to read device /heartbeat request")
+		return
+	}
+	var h api.DeviceHeartbeat
+	err = json.Unmarshal(data, &h)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Failed to unmarshal /heartbeat request"))
+		return
+	}
+	tx := db.MustBegin()
+	tx.MustExec(heartbeatQuery, h.DeviceID, Heartbeat, time.Now())
+	tx.Commit()
+}
+
 func deviceDispenseHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	data, err := ioutil.ReadAll(r.Body)
@@ -46,6 +74,7 @@ func deviceDispenseHandler(db *sqlx.DB, w http.ResponseWriter, r *http.Request) 
 	err = json.Unmarshal(data, &t)
 	if err != nil {
 		w.WriteHeader(500)
-		w.Write([]byte("Failed to read device /dispense request"))
+		w.Write([]byte("Failed to unmarshal /dispense request"))
+		return
 	}
 }
