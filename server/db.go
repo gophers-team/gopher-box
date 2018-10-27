b313eefcfb82015125e0f67f35ea878ac0e79a0b
Dixie Flatline <vitalisbox@gmail.com>
2018-10-27 15:41:35 +0300

    Tables 4 dispensings

--- server/db.go
+++ server/db.go
@@ -3,7 +3,6 @@ package main
 import (
 	"time"
 
-	"github.com/gophers-team/gopher-box/api"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -15,29 +14,21 @@ const (
 	PillsDispensed
 )
 
-type Event struct {
-	Id          int `db:"id"`
-	DeviceId    api.DeviceID
-	Type        EventType `db:"event_type"`
-	Description string    `db:"description"`
-	Timestamp   time.Time `db:"timestamp"`
-	CreatedAt   time.Time `db:"created_at"`
-}
-
 type DispensingPlan struct {
-	Id        int
-	Name      string
+	Id        int       `db:"id"`
+	Name      string    `db:"name"`
 	CreatedAt time.Time `db:"created_at"`
 }
 
 type DispensingSchedule struct {
-	Id           int
-	PlanId       int `db:"plan_id"`
-	PillId       int `db:"pill_id"`
-	Amount       int
-	DispenseDow  int       `db:"dispense_dow"`
-	DispenseTime time.Time `db:"dispense_time"`
-	CreatedAt    time.Time `db:"created_at"`
+	Id               int       `db:"id"`
+	PlanId           int       `db:"plan_id"`
+	PillId           int       `db:"pill_id"`
+	Amount           int       `db:"amount"`
+	DispenseDow      int       `db:"dispense_dow"`
+	ScheduleDuration int       `db:"schedule_duration"`
+	DispenseTime     time.Time `db:"dispense_time"`
+	CreatedAt        time.Time `db:"created_at"`
 }
 
 func InitDb(dbFile string) (*sqlx.DB, error) {
--- server/schema.go
+++ server/schema.go
@@ -1,17 +1,29 @@
 package main
 
 var schema = `
-DROP TABLE IF EXISTS events;
 DROP TABLE IF EXISTS devices;
+DROP TABLE IF EXISTS device_dispensings;
 DROP TABLE IF EXISTS pills;
 DROP TABLE IF EXISTS dispensing_plans;
 DROP TABLE IF EXISTS dispensing_schedule;
-DROP TABLE IF EXISTS operations;
 
 CREATE TABLE devices (
     id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	plan_id    	INTEGER NOT NULL,
 	name 		VARCHAR(255) NOT NULL,
-    created_at  DATETIME
+    created_at  DATETIME,
+	FOREIGN KEY (plan_id) REFERENCES plans(id)
+);
+
+CREATE TABLE device_dispensings (
+	id          	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    device_id     	INTEGER NOT NULL,
+    operation_id	INTEGER NOT NULL,
+	schedule_id		INTEGER,
+	pills_dispensed	INTEGER,
+    created_at  	DATETIME,
+	FOREIGN KEY (device_id) REFERENCES devices(id)
+	FOREIGN KEY (schedule_id) REFERENCES dispensing_schedule(id)
 );
 
 CREATE TABLE pills (
@@ -27,28 +39,16 @@ CREATE TABLE dispensing_plans (
 );
 
 CREATE TABLE dispensing_schedule (
-    id          	INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    plan_id     	INTEGER NOT NULL,
-	pill_id			INTEGER NOT NULL,
-	amount			INTEGER NOT NULL,
-	dispense_dow	INTEGER NOT NULL,
-	dispense_time   DATETIME NOT NULL,
-	created_at  	DATETIME,
+	id          		INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    plan_id     		INTEGER NOT NULL,
+	pill_id				INTEGER NOT NULL,
+	amount				INTEGER NOT NULL,
+	dispense_dow		INTEGER NOT NULL,
+	dispense_time   	DATETIME NOT NULL,
+	schedule_duration 	INTEGER NOT NULL,
+	created_at  		DATETIME,
 	FOREIGN KEY (plan_id) REFERENCES plans(id)
 	FOREIGN KEY (pill_id) REFERENCES pills(id)
 );
 
-CREATE TABLE events (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	device_id	VARCHAR(127) NOT NULL,
-    event_type  UNSIGNED INT8 NOT NULL,
-    description VARCHAR(255),
-    timestamp   DATETIME,
-    created_at  DATETIME
-);
-
-CREATE TABLE operations (
-	id        INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-	device_id VARCHAR(127) NOT NULL
-);
 `
