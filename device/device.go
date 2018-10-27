59fe8e597f24a1134d0de461ec2d1e09927212fd
Sergey Elantsev <elantsev.s@yandex.ru>
2018-10-27 03:58:28 +0300

    real moving device

--- device/device.go
+++ device/device.go
@@ -1,38 +1,54 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"os"
+	"time"
 
-	"github.com/urfave/cli"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
 )
 
 func main() {
-	app := cli.NewApp()
+	tty := flag.String("tty", "/dev/ttyACM0", "arduino tty")
+	in1 := flag.String("in1", "8", "in1")
+	in2 := flag.String("in2", "9", "in2")
+	in3 := flag.String("in3", "10", "in3")
+	in4 := flag.String("in4", "11", "in4")
+	stepsPerRev := flag.Uint("steps-per-rev", 2038, "steps per rev")
+	step := flag.Int("step", 2038, "step")
+	rpm := flag.Uint("rpm", 10, "rpm speed")
+	revRpm := flag.Uint("rev-rpm", 10, "rev rpm speed")
+	flag.Parse()
 
-	app.Commands = []cli.Command{
-		{
-			Name:    "event",
-			Aliases: []string{"e"},
-			Usage:   "send event",
-			Action: sendEvent(),
-		},
-		{
-			Name:    "start",
-			Aliases: []string{"s"},
-			Usage:   "start device",
-			Action: func(c *cli.Context) error {
-				log.Println("Device is starting...")
-				return nil
-			},
-		},
+	pins := [...]string{*in1, *in2, *in3, *in4}
+
+	firmataAdaptor := firmata.NewAdaptor(*tty)
+	motor := gpio.NewStepperDriver(firmataAdaptor, pins, gpio.StepperModes.SinglePhaseStepping, *stepsPerRev)
+
+	work := func() {
+		for {
+			motor.SetSpeed(*rpm)
+			err := motor.Move(*step)
+			if err != nil {
+				log.Println("move error: %v", err)
+			}
+			go sendEvent()
+			time.Sleep(time.Second)
+			motor.SetSpeed(*revRpm)
+			motor.Move(-*step)
+		}
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	robot := gobot.NewRobot("bot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{motor},
+		work,
+	)
+
+	robot.Start()
 }
 
 func sendEvent() error {
--- go.mod
+++ go.mod
@@ -2,8 +2,21 @@ module github.com/gophers-team/gopher-box
 
 require (
 	github.com/boltdb/bolt v1.3.1
+	github.com/creack/goselect v0.0.0-20180501195510-58854f77ee8d // indirect
+	github.com/go-ble/ble v0.0.0-20181002102605-e78417b510a3 // indirect
 	github.com/gorilla/mux v1.6.2
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/sigurn/crc8 v0.0.0-20160107002456-e55481d6f45c // indirect
+	github.com/sigurn/utils v0.0.0-20151230205143-f19e41f79f8f // indirect
 	github.com/urfave/cli v1.20.0
+	go.bug.st/serial.v1 v0.0.0-20180827123349-5f7892a7bb45 // indirect
+	gobot.io/x/gobot v1.12.0
+	golang.org/x/sys v0.0.0-20181026203630-95b1ffbd15a5 // indirect
 )
